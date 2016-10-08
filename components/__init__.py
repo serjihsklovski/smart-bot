5b07e76bc258c3286762e3d597485484cba86f6c
Serjih Sklovski <serjihsklovski@gmail.com>
2016-10-08 22:13:37 +0300

    `components/__init__.py`: `drop_database` function was released. script `init.py` creates full environment in mysql

--- components/__init__.py
+++ components/__init__.py
@@ -11,3 +11,15 @@ def create_database(cursor, new_database_name):
         """.format(new_database_name)
 
     cursor.execute(query)
+
+
+def drop_database(cursor, database_name):
+    """
+    drops database from mysql
+
+    :param cursor: mysql.connector.connection.cursor object
+    :param database_name: string
+    :return: None
+    """
+    query = 'DROP DATABASE `{}`'.format(database_name)
+    cursor.execute(query)
--- init.py
+++ init.py
@@ -5,6 +5,7 @@ all you have to do is input your mysql username, password and token of your bot.
 import mysql.connector as mc
 import telebot as tb
 from components import create_database
+from components import drop_database
 
 print('smart-bot: "input your mysql username"')
 username = input('you: ')
@@ -52,12 +53,84 @@ else:
         else:
             print(
                 (
-                    'smart-bot: "OK - database for {0} was created. ' +
+                    'smart-bot: "OK - database for @{0} was created. ' +
                     'it is called `{1}`"'
                 ).format(bot_info.username, smart_bot_database_name)
             )
 
-            # create tables here
+            conn.database = smart_bot_database_name
+
+            tables = {
+                'info':
+                    'CREATE TABLE `info` (' +
+                    '`name` varchar(128) NOT NULL UNIQUE, ' +
+                    '`value` varchar(255) DEFAULT NULL' +
+                    ') ENGINE=InnoDB DEFAULT CHARSET=utf8',
+
+                'command':
+                    'CREATE TABLE `command` (' +
+                    '`id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, ' +
+                    '`name` varchar(255) NOT NULL UNIQUE, ' +
+                    '`description` text' +
+                    ') ENGINE=InnoDB DEFAULT CHARSET=utf8',
+
+                'status':
+                    'CREATE TABLE `status` (' +
+                    '`id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, ' +
+                    '`name` varchar(128) NOT NULL UNIQUE, ' +
+                    '`description` text' +
+                    ') ENGINE=InnoDB DEFAULT CHARSET=utf8',
+
+                'language':
+                    'CREATE TABLE `language` (' +
+                    '`id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, ' +
+                    '`name` varchar(12) NOT NULL UNIQUE' +
+                    ') ENGINE=InnoDB DEFAULT CHARSET=utf8',
+
+                'contact':
+                    'CREATE TABLE `contact` (' +
+                    '`id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, ' +
+                    '`telegram_id` int(11) NOT NULL UNIQUE, ' +
+                    '`username` varchar(255) DEFAULT NULL, ' +
+                    '`first_name` varchar(255) DEFAULT NULL, ' +
+                    '`last_name` varchar(255) DEFAULT NULL, ' +
+                    '`rating` int(11) NOT NULL, ' +
+                    '`status_id` int(11) DEFAULT NULL, ' +
+                    '`language_id` int(11) DEFAULT NULL' +
+                    ') ENGINE=InnoDB DEFAULT CHARSET=utf8',
+
+                'mention':
+                    'CREATE TABLE `mention` (' +
+                    '`id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, ' +
+                    '`value`  varchar(255) NOT NULL UNIQUE, ' +
+                    '`description` text' +
+                    ') ENGINE=InnoDB DEFAULT CHARSET=utf8',
+
+                'replica':
+                    'CREATE TABLE `replica` (' +
+                    '`id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, ' +
+                    '`value` text, ' +
+                    '`mention_id` int(11) DEFAULT NULL, ' +
+                    '`language_id` int(11) DEFAULT NULL' +
+                    ') ENGINE=InnoDB DEFAULT CHARSET=utf8'
+            }
+
+            for k in tables:
+                try:
+                    print('smart-bot: "creating table `{0}`.`{1}`"'.format(
+                        smart_bot_database_name, k
+                    ))
+
+                    curs.execute(tables[k])
+
+                except mc.Error as err:
+                    print('smart-bot: "FAILED - {}"'.format(err))
+                    drop_database(curs, smart_bot_database_name)
+                    print('smart-bot: "init - cancelled"')
+                    break
+
+            else:
+                print('smart-bot: "OK - all tables were created successfully"')
 
         finally:
             curs.close()
