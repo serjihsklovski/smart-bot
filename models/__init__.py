a0cb469bc0a7261d972125c00212c7894e1e0686
Serjih Sklovski <serjihsklovski@gmail.com>
2016-10-09 19:16:50 +0300

    added `models/__init__.py` basic `Model` class

---
+++ models/__init__.py
@@ -0,0 +1,40 @@
+import mysql.connector as mc
+import configparser as cnf
+from pathlib import Path
+
+
+class ConfigFileException(Exception):
+    pass
+
+
+class Model:
+    def __init__(self, table_name):
+        path = Path('config.ini')
+
+        if path.is_file():
+            config = cnf.ConfigParser()
+            config.read_file(open('config.ini'))
+
+            if 'login' not in config:
+                raise ConfigFileException
+
+            for option in ['host', 'database', 'user', 'password']:
+                if option not in config['login']:
+                    raise ConfigFileException
+
+                else:
+                    setattr(self, '_' + option, config['login'][option])
+
+            try:
+                self._conn = mc.connect(self._host,
+                                        self._database,
+                                        self._user,
+                                        self._password)
+
+            except mc.Error as err:
+                print('smart-bot: "FAILED - {}"'.format(err))
+
+        else:
+            raise ConfigFileException
+
+        self._table_name = table_name
